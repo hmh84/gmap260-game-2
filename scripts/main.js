d30d69f40f8a8617d8d46629a29dd3f224b89de4
Hunter Hampton <hmh84@drexel.edu>
2020-11-20 17:42:04 -0500

    Countries

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 // =========================
-// HELPER FUNCTIONS/STUFF
+// GENERAL FUNCTIONS
 // =========================
 
 const docQ = document.querySelector.bind(document),
@@ -14,6 +14,10 @@ function random_chance(chance) { // Chance param is a decimal
     }
 }
 
+// =========================
+// UI STATS
+// =========================
+
 // Slider
 const slider = docQ('#slider'),
     slider_val = docQ('#slider_val');
@@ -25,17 +29,6 @@ function update_slider_val() {
     slider_val.innerText = 'Slider Value = ' + slider.value;
 }
 
-// Map
-
-map_paths = docQA('#Layer_1 path');
-
-map_paths.forEach(path => {
-    path.addEventListener('click', (e) => {
-        e.target.style.fill = '#ffffff';
-        console.log(path);
-    })
-});
-
 // Progress Bar
 
 const cure_progress_bar = docQ('#cure_progress_bar')
@@ -43,8 +36,56 @@ const cure_progress_bar = docQ('#cure_progress_bar')
 function update_cure_progress_bar(value) {
     // Used to update UI element of the cure progress
     // Param 'value' should be an integer of 0-100
-    // update_cure_progress_bar
+    // Ex. update_cure_progress_bar('50');
     cure_progress_bar.style.width = `${value}%`;
 }
 
-update_cure_progress_bar('50'); // 50%
+// =========================
+// COUNTRIES
+// =========================
+
+var countries = [
+    {
+        name: 'USA',
+        defaults: {
+            population: 328200000,
+            budget: 500000000,
+            coop: 60,
+        }
+    },
+    {
+        name: 'China',
+        defaults: {
+            population: 1393000000,
+            budget: 500000000,
+            coop: 90,
+        }
+    },
+    {
+        name: 'Germany',
+        defaults: {
+            population: 300000000,
+            budget: 83000000,
+            coop: 70,
+        }
+    },
+    {
+        name: 'Angola',
+        defaults: {
+            population: 300000000,
+            budget: 30000000,
+            coop: 40,
+        }
+    },
+];
+
+var my_country = [
+    {
+        name: 'USA',
+        defaults: {
+            population: 328200000,
+            budget: 500000000,
+            coop: 60,
+        }
+    }
+]
